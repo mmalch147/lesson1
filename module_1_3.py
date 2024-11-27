b458b38ba9b964ffb269abfe755059795040422c
Mmalch147 <Mmalch147@gmail.com>
2024-11-27 16:24:02 +0300

    Практическое задание по теме "Переменные"

---
+++ module_1_3.py
@@ -0,0 +1,32 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+name = "Mariya"
+print(name, type(name))
+age = 32
+print(age, type(age))
+new_age = 23
+print(new_age, type(new_age))
+is_student = "true"
+print(is_student, type(is_student))
+the_number_of_completed_DZ = 12
+
+The_number_of_hours_spent = 1.5
+
+Course_name = 'Python'
+
+Time_per_task = The_number_of_hours_spent/the_number_of_completed_DZ
+print("Курс:", Course_name, "всего задач:", the_number_of_completed_DZ, ", затрачено часов:", The_number_of_hours_spent, ", среднее время выполнения", Time_per_task)
